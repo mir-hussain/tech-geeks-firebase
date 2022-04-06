fa95f2d7be1faafc4b597aa08e63fde965a55abd
mir-hussain <mirhussainmurtaza@gmail.com>
2022-04-06 14:28:42 +0600

    first commit

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./AuthForm.css";
+import GoogleLogo from "../../Assets/Image/google.svg";
+
+const Login = () => {
+  return (
+    <div className='auth-form-container '>
+      <div className='auth-form'>
+        <h1>Login</h1>
+        <div>
+          <div className='input-field'>
+            <label htmlFor='email'>Email</label>
+            <div className='input-wrapper'>
+              <input type='email' name='email' id='email' />
+            </div>
+          </div>
+          <div className='input-field'>
+            <label htmlFor='password'>Password</label>
+            <div className='input-wrapper'>
+              <input type='password' name='password' id='password' />
+            </div>
+          </div>
+          <button type='submit' className='auth-form-submit'>
+            Login
+          </button>
+        </div>
+        <p className='redirect'>
+          New to Tech Geeks? <span>Create New Account</span>
+        </p>
+        <div className='horizontal-divider'>
+          <div className='line-left' />
+          <p>or</p>
+          <div className='line-right' />
+        </div>
+        <div className='input-wrapper'>
+          <button className='google-auth'>
+            <img src={GoogleLogo} alt='' />
+            <p> Continue with Google </p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
